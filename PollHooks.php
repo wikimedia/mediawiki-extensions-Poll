1242afd0efbc9244f04ed38813bd76b2ab095808
Zoranzoki21 <zorandori4444@gmail.com>
2020-01-12 16:37:31 +0100

    Rename archives/ to sql/
    sql files aren't archives, they are for updating database
    
    Change-Id: Iac13e0f8e0892a4348c25421e1ab3a35e531b2aa

--- PollHooks.php
+++ PollHooks.php
@@ -12,23 +12,23 @@ class PollHooks {
 		$base = __DIR__;
 		if ( $updater->getDB()->getType() == 'mysql' ) {
 			// "poll"-Table: All infomation about the polls
-			$updater->addExtensionUpdate( array( 'addTable', 'poll', "$base/archives/Poll.sql", true ) ); // Initial install tables
-			$updater->addExtensionUpdate( array( 'addField', 'poll', 'creater', "$base/archives/patch-creater.sql", true ) ); // Add creater
-			$updater->addExtensionUpdate( array( 'addField', 'poll', 'dis', "$base/archives/patch-dis.sql", true ) ); // Add dis
-			$updater->addExtensionUpdate( array( 'addField', 'poll', 'multi', "$base/archives/patch-multi.sql", true ) ); // Add multi
-			$updater->addExtensionUpdate( array( 'addField', 'poll', 'ip', "$base/archives/patch-ip.sql", true ) ); // Add ip
-			$updater->addExtensionUpdate( array( 'addField', 'poll', 'runtime', "$base/archives/patch-runtime.sql", true ) ); // Add runtime
-			$updater->addExtensionUpdate( array( 'addField', 'poll', 'starttime', "$base/archives/patch-starttime.sql", true ) ); // Add starttime
-			$updater->addExtensionUpdate( array( 'addField', 'poll', 'end', "$base/archives/patch-end.sql", true ) ); // Add end
+			$updater->addExtensionUpdate( array( 'addTable', 'poll', "$base/sql/Poll.sql", true ) ); // Initial install tables
+			$updater->addExtensionUpdate( array( 'addField', 'poll', 'creater', "$base/sql/patch-creater.sql", true ) ); // Add creater
+			$updater->addExtensionUpdate( array( 'addField', 'poll', 'dis', "$base/sql/patch-dis.sql", true ) ); // Add dis
+			$updater->addExtensionUpdate( array( 'addField', 'poll', 'multi', "$base/sql/patch-multi.sql", true ) ); // Add multi
+			$updater->addExtensionUpdate( array( 'addField', 'poll', 'ip', "$base/sql/patch-ip.sql", true ) ); // Add ip
+			$updater->addExtensionUpdate( array( 'addField', 'poll', 'runtime', "$base/sql/patch-runtime.sql", true ) ); // Add runtime
+			$updater->addExtensionUpdate( array( 'addField', 'poll', 'starttime', "$base/sql/patch-starttime.sql", true ) ); // Add starttime
+			$updater->addExtensionUpdate( array( 'addField', 'poll', 'end', "$base/sql/patch-end.sql", true ) ); // Add end
 
 			// "poll_answer"-Table: The answer of the users
-			$updater->addExtensionUpdate( array( 'addTable', 'poll_answer', "$base/archives/Poll-answer.sql", true ) ); // Initial answer tables
-			$updater->addExtensionUpdate( array( 'addField', 'poll_answer', 'user', "$base/archives/patch-user.sql", true ) ); // Add user
-			$updater->addExtensionUpdate( array( 'addField', 'poll_answer', 'vote_other', "$base/archives/patch-vote_other.sql", true ) ); // Add vote_other
-			$updater->addExtensionUpdate( array( 'addField', 'poll_answer', 'ip', "$base/archives/patch-answer-ip.sql", true ) ); // Add ip
+			$updater->addExtensionUpdate( array( 'addTable', 'poll_answer', "$base/sql/Poll-answer.sql", true ) ); // Initial answer tables
+			$updater->addExtensionUpdate( array( 'addField', 'poll_answer', 'user', "$base/sql/patch-user.sql", true ) ); // Add user
+			$updater->addExtensionUpdate( array( 'addField', 'poll_answer', 'vote_other', "$base/sql/patch-vote_other.sql", true ) ); // Add vote_other
+			$updater->addExtensionUpdate( array( 'addField', 'poll_answer', 'ip', "$base/sql/patch-answer-ip.sql", true ) ); // Add ip
 
 			// "poll_start_log"-Table: Time with last run of Poll::start()
-			$updater->addExtensionUpdate( array( 'addTable', 'poll_start_log', "$base/archives/Poll-start-log.sql", true ) ); // Initial start_log tables
+			$updater->addExtensionUpdate( array( 'addTable', 'poll_start_log', "$base/sql/Poll-start-log.sql", true ) ); // Initial start_log tables
 		}
 	}
 }
